617fc198a5742bf40f3661fa16f76268d1be85d6
Egor Rumyantsev <RumyantsevGT@ya.ru>
2020-11-12 21:26:15 +0300

    Update v.js

--- @/v.js
+++ @/v.js
@@ -383,7 +383,7 @@ var COLOR = [
         fn: squares,
         sort: sortTopMid
     }],
-    curr_color = 6,
+    curr_color = 7,
     playing = !0,
     curr_vis = 4,
     color = COLOR[curr_color],
