d5e4f82a73912a477cd8d621ea282bd868631e36
Egor Rumyantsev <RumyantsevGT@ya.ru>
2021-08-02 00:40:18 +0300

    Update tetris.js

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -891,7 +891,7 @@ window.onkeydown = function (event) {
  			html = replaceAll(html, "," + (c + 1), ",<font color=\"" + colors[c] + "\">" + (c + 1) + "</font>");
  			html = replaceAll(html, (c + 1) + ",", "<font color=\"" + colors[c] + "\">" + (c + 1) + "</font>,");
  		}
- 		html += "<br />Drop Time: " + speed;
+ 		html += "<br /> время : " + speed;
  		if (ai) {
  			html += "<br />Ход: " + movesTaken + "/" + moveLimit;
  			html += "<br />Генерация: " + generation;
